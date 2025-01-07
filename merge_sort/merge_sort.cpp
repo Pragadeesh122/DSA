af878112a45666fdbc520aa8a68dc278268bbfcd
Pragadeesh122 <pragan189@gmail.com>
2025-01-08 01:19:34 +0530

    quicksort

--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -96,10 +96,88 @@ void recursiveBubble(vector<int>& nums, int size){
 
 }
 
+void insertSort(vector<int>& nums){
+  
+    for(int i = 1; i < nums.size(); i++){
+        int j =i;
+        while(j > 0 && nums[j-1] > nums[j]){
+           
+            int temp = nums[j-1];
+            nums[j-1] = nums[j];
+            nums[j] = temp;     
+            
+            j--;
+
+        }
+    }
+        cout << "The sorted array is:" << endl;
+    for(auto i : nums){
+        cout << i << endl;
+    }
+
+}
+
+
+void recursiveInsertion(vector<int>& nums, int n){
+
+    if(n >= nums.size() -1) {
+        cout << "The recursive insertion sort:" << endl;
+        for(auto i : nums){
+            cout << i << endl;
+        }
+        return;
+    }
+    int j = n;
+    while(j > 0 && nums[j-1] > nums[j]){
+         int temp = nums[j-1];
+            nums[j-1] = nums[j];
+            nums[j] = temp;     
+            
+            j--;
+    }
+    recursiveInsertion(nums,n+1);
+
+}
+
+int partitionIndex(vector<int>& nums, int low, int high){
+    int pivot = nums[low];
+    int i = low;
+    int j = high;
+    while( i < j){
+        while(nums[i] <= pivot && i < high){
+            i++;
+        }
+        while(nums[j] >= pivot && j > low){
+            j--;
+        }
+
+        if(i < j){
+            swap(nums[i], nums[j]);
+        }
+    }
+    swap(nums[low], nums[j]);
+    return j;
+    
+   
+    
+}
+void quickSort(vector<int>& nums, int low, int high){
+    if(low >= high){
+        cout << "This is the quick sorted array:" << endl;
+        for(auto i : nums){
+            cout << i << endl;
+        }
+        return;
+    }
+    int p_index = partitionIndex(nums,low,high);
+    quickSort(nums, low, p_index);
+    quickSort(nums, p_index+1, high);
+}
+
 int main(){
    
   vector<int> nums = {14,15,9,12,6,8,13};
-//   mergeSort(nums,0,nums.size()-1);
-recursiveBubble(nums,nums.size());
+  quickSort(nums,0, nums.size()-1);
+
 
 }
